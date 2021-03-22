ff640f4f646dd8307a949b96076cf5c3da6b8554
deepa977 <deepa977@gmail.com>
2021-03-22 15:03:38 +0530

    Deepa Created new project CalculatorProject

---
+++ CalculatorProject/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject
+{
+    class Calculator
+    {
+        int x;
+        int y;
+        int result;
+    }
+}
