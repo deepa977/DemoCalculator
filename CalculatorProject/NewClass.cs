74aac9d12590ef51638d818cee0e471b970f1d29
deepa977 <deepa977@gmail.com>
2021-03-23 09:32:21 +0530

    Newclass created by Deepa with a new method Test()

---
+++ CalculatorProject/NewClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject
+{
+    class NewClass
+    {
+        public void Test()
+        {
+            Console.WriteLine("Deepa New Method.....");
+        }
+    }
+}
